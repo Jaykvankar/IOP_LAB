48d63b709086dbd50dd8fae438358c1deba42238
Jaykvankar <vankarjayk@gmail.com>
2024-09-05 15:15:31 +0530

    Update LAB2_Q19.c

--- LAB2_Q19.c
+++ LAB2_Q19.c
@@ -5,7 +5,6 @@ int main()
     char ch, c;
     printf("Enter a character: ");
     scanf("%c", &ch);
-    c=(ch>='a'?(ch<='z'):(printf("the given character is not a small case latter")));
-    printf("the given %c is a small case character", ch);
+    c=(ch>='a'&& ch<='z')?printf("the given %c is a small case character", ch):(printf("the given character is not a small case latter"));
     return 0;
 }
