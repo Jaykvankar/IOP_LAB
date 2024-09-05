4264b4d404fe06e62350f2c84de99cc1deb36400
Jaykvankar <vankarjayk@gmail.com>
2024-09-05 14:07:13 +0530

    Delete 18.c

--- 18.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float num1, num2, num3;
-    printf("Enter three numbers");
-    scanf("%f %f %f", &num1, &num2, &num3);
-    float max = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
-    printf("\nThe maximum number is %.2f", max);
-    return 0;
-}
