c33713dd0afb75ca6771ad4dd15cb1d4d94e69ef
Jaykvankar <vankarjayk@gmail.com>
2024-09-12 13:28:33 +0530

    Update LAB3_Q33.c

--- LAB3/LAB3_Q33.c
+++ LAB3/LAB3_Q33.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 int main()
 {
-    int n, a, min = 0, max, i = 1, x;
+    int n, a, min, max , i = 1, x;
     printf("How many numbers do you want to compare?\n");
     scanf("%d", &n);
     printf("Enter a number\n");
     scanf("%d", &a);
-    max = a;
+    min = a;
     while (i < n)
     {
         printf("Enter another number\n");
         scanf("%d", &x);
-        if (x>max)
+        if (x<min)
+            {
+            max=min;
+            min=x;
+            }
+        else if (x>min)
+            {
+            max=x;
+            }
+        else if (x>max)
             {
             min=max;
             max=x;
             }
-        else if (x<min||min==max)
+        else if (x>min&&x<max)
             {
             min=x;
             }
