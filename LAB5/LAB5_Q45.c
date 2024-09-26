df0d50ad92ea5c55ad0363f1025a1892bd05c9e0
Jaykvankar <vankarjayk@gmail.com>
2024-09-26 15:18:52 +0530

    Update LAB5_Q45.c

--- LAB5/LAB5_Q45.c
+++ LAB5/LAB5_Q45.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
     srand(time(NULL));
     int luckyNumber = rand() % 100 + 1;
-
     int guess;
     int attempts = 0;
-
     printf("Welcome to the Numeric Guessing Game!\n");
     printf("I've picked a lucky number between 1 and 100. Can you guess it?\n");
-
     do {
         printf("Enter your guess: ");
         scanf("%d", &guess);
         attempts++;
-
         if (guess == luckyNumber)
         {
             printf("Congratulations! You guessed it right in %d attempts.\n", attempts);
@@ -29,6 +26,5 @@ int main() {
             printf("Try a lower number.\n");
         }
     } while (1);
-
     return 0;
 }
