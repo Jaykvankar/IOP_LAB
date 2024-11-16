9827dc249517a7615be1a64cd1cfc30cdc9a3fd3
Jaykvankar <vankarjayk@gmail.com>
2024-11-16 15:13:12 +0530

    Create Q1.c

---
+++ LAB1/Q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int p, r, t, si;
+    printf("Enter principal value, rate, time");
+    scanf("%d%d%d", &p, &r, &t);
+    si=p*r*t/100;
+    printf("Simple Intrest=%d\n", si);
+    return 0;
+}
